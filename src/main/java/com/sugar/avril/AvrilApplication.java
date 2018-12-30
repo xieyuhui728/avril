dd30b7f5575aa4e9e1218967c06a6dcda5dea845
xieyuhui <xieyuhu728@qq.comi>
2018-12-30 22:01:58 +0800

    frist commit

---
+++ src/main/java/com/sugar/avril/AvrilApplication.java
@@ -0,0 +1,15 @@
+package com.sugar.avril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AvrilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvrilApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/sugar/avril/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.sugar.avril;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AvrilApplication.class);
+    }
+
+}
+
