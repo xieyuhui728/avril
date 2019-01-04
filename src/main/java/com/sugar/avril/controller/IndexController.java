a4c9a96ff049fa094e6541eac14ee27e7f03ce23
xieyuhui <xieyuhu728@qq.comi>
2019-01-04 20:51:07 +0800

    newly thymeleaf

---
+++ src/main/java/com/sugar/avril/controller/IndexController.java
@@ -0,0 +1,31 @@
+package com.sugar.avril.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: xieyuhui
+ * @Date: 2018-12-30 22:44
+ * @Description: 首页控制类
+ */
+@RestController
+public class IndexController {
+
+    @Value("${spring.indexInfo}")
+    private String message;
+
+    /**
+     *
+     * Description: 
+     *
+     * @param:
+     * @return: 
+     * @auther: xieyuhui
+     * @date: 2018-12-30 22:52
+     */
+    @RequestMapping("/")
+    public String index() {
+        return message;
+    }
+}
---
+++ src/main/java/com/sugar/avril/controller/ThymeleafController.java
@@ -0,0 +1,22 @@
+package com.sugar.avril.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @Auther: xieyuhui
+ * @Date: 2018-12-31 15:09
+ * @Description:
+ */
+@Controller
+@RequestMapping("/thymeleaf")
+public class ThymeleafController {
+
+    @RequestMapping("/home")
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("homeMessage","this is thymeleaf home~");
+        return modelAndView;
+    }
+}
